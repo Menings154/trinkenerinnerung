b8f3eb8910c437cf5117ef02a70d62aa613f2c7e
menings windows <menings.git@gmail.com>
2022-01-07 11:18:01 +0100

    Whatsappautomatisierung (clickposition und Zeit davor wurden in pywhatkit.whats gändert.

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from pywhatkit.whats import sendwhatmsg_instantly
+
+sendwhatmsg_instantly(phone_no='+4915172680214', 
+                     message='Freundliche Erinnerung etwas zu trinken :)',
+                     wait_time=15,
+                     tab_close=True,
+                     close_time=5 )
